487883e532d1af80d2d044c4b5c4feb9fd9dbd5c
BOTLANNER <16349308+BOTLANNER@users.noreply.github.com>
2023-01-31 11:00:07 +0200

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,12 +1,21 @@
-let path = window.location.pathname;
-const pathParts = path.split('/');
-let pathIndex = 0;
-if (pathParts.includes('versions')) {
-    pathIndex = pathParts.indexOf('versions') + 1;
-} else {
-    pathIndex = pathParts.indexOf('docs') + 1;
+function redirect() {
+    let path = window.location.pathname;
+    const pathParts = path.split('/');
+    let pathIndex = 0;
+    if (pathParts.includes('versions')) {
+        // Redirect to version specific home page
+        pathIndex = pathParts.indexOf('versions') + 1;
+    } else {
+        // Redirect to specific docs home page
+        pathIndex = pathParts.indexOf('docs') + 1;
+    }
+    path = pathParts.slice(0, pathIndex + 1).join('/');
+    if (path !== window.location.pathname) {
+        window.location.pathname = path;
+    } else {
+        // Fallback redirect to landing page
+        window.location.pathname = '/open-source'; 
+    }
 }
-path = pathParts.slice(0, pathIndex + 1).join('/');
-if (path !== window.location.pathname) {
-    window.location.pathname = path;
-}
+
+redirect();

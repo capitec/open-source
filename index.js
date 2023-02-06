18ace845c18e6fa98f9a5252134678b722768e9f
BOTLANNER <16349308+BOTLANNER@users.noreply.github.com>
2023-02-06 13:54:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,44 +2,49 @@ const mainDiv = document.getElementById('main');
 const baseRef = window.location.pathname.slice(0, -1);
 const template = document.querySelector('template');
 
-// Fetch content from repos.json file.
-const requestInit = {
-    mode: `cors`,
-    method: `GET`,
-    credentials: `same-origin`,
-    headers: {
-        'Pragma': `no-cache`,
-        'Cache-Control': `no-store`
-    }
-};
-
-const response = await fetch(`${baseRef}/repos.json`, requestInit);
-let structure = await response.text();
-
-if (structure) {
-    structure = JSON.parse(structure);
-} else {
-    structure = {
-        repos: []
+(async () => {
+    // Fetch content from repos.json file.
+    const requestInit = {
+        mode: `cors`,
+        method: `GET`,
+        credentials: `same-origin`,
+        headers: {
+            'Pragma': `no-cache`,
+            'Cache-Control': `no-store`
+        }
     };
-}
 
-// Listen for event where OS color scheme changes and re-render repos.
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-    mainDiv.innerHTML = '';
+    const response = await fetch(`${baseRef}/repos.json`, requestInit);
+    let structure = await response.text();
+
+    if (structure) {
+        structure = JSON.parse(structure);
+    } else {
+        structure = {
+            repos: []
+        };
+    }
+
+    // Listen for event where OS color scheme changes and re-render repos.
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', renderRepos);
+
     renderRepos();
-});
 
-renderRepos();
-
-function renderRepos() {
-    if ('content' in document.querySelector('template')) {
+    function renderRepos() {
+        mainDiv.innerHTML = '';
+        const templateSupported = !!template.content;
         for (const repo of structure.repos) {
             // console.log(repo);
 
             // Repo docs path inferred from repo name.
             const repoPath = `${baseRef}/docs/${repo.name}`;
-            const clonedNode = template.content.cloneNode(true);
+
+
+            const clonedNode = templateSupported ? template.content.cloneNode(true) : document.createElement('div');
+            if (!templateSupported) {
+                clonedNode.innerHTML = template.innerHTML;
+            }
+
             const gitHubMarkPath = `github-mark${window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? `-white` : ''}.png`;
 
             clonedNode.querySelector('a').href = repoPath;
@@ -55,9 +60,28 @@ function renderRepos() {
             clonedNode.querySelector('[data-name]').innerHTML = repo.display ?? repo.name;
             clonedNode.querySelector('[data-desc]').innerHTML = repo.description ?? '';
 
-            mainDiv.appendChild(clonedNode);
+            if (templateSupported) {
+                mainDiv.appendChild(clonedNode);
+            } else {
+                moveElements(clonedNode, mainDiv);
+            }
+
         }
-    } else {
-        // TODO
+        if (!templateSupported) {
+            template.remove();
+        }
+    }
+})();
+
+function dragStartHandler(ev) {
+    const target = ev.target;
+    const boundingRect = target.getBoundingClientRect();
+    ev.dataTransfer.setDragImage(ev.target, (boundingRect.width ?? target.offsetWidth) / 2, (boundingRect.height ?? target.offsetHeight) / 2);
+}
+
+function moveElements(oldParent, newParent) {
+    while (oldParent.childNodes.length > 0) {
+        newParent.appendChild(oldParent.childNodes[0]);
     }
 }
+
